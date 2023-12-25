f2591674546da4dea5ca072e8d9933a93a1aef5a
winchestor2001 <sbexruz2001@gmail.com>
2023-12-25 11:36:28 +0500

    add serializer for lesson articles

--- student_platform/serializers.py
+++ student_platform/serializers.py
@@ -1,11 +1,28 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from .models import Course, Lesson
+from .models import Course, Lesson, Article
+
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
 
 
 class LessonSerializer(ModelSerializer):
+
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = ['id', 'course', 'number', 'title', 'price', 'paid']
+
+    @staticmethod
+    def get_lesson_articles(obj):
+        result = ArticleSerializer(instance=obj.article_set.all(), many=True)
+        return result.data
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['lesson_articles'] = self.get_lesson_articles(instance)
+        return data
 
 
 class CourseSerializer(ModelSerializer):
