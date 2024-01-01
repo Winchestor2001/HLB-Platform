4ee8a605c8f621b01f44afa600a735ca8b0cc8b7
Bexruz Sayfiddinov <75200506+Winchestor2001@users.noreply.github.com>
2024-01-01 18:07:53 +0500

    Update serializers.py

--- student_platform/serializers.py
+++ student_platform/serializers.py
@@ -27,7 +27,12 @@ class LessonSerializer(ModelSerializer):
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
+
+        user = self.context['request'].user
+        student_course = instance.course.studentcourse_set.filter(student=user)
+
         data['lesson_articles'] = self.get_lesson_articles(instance)
+        data['is_visible'] = student_course.exists()
         return data
 
 
@@ -52,23 +57,23 @@ class StudentAddCourseSerializer(ModelSerializer):
     def create(self, validated_data):
         student = validated_data['student']
         course = validated_data['course']
+        student_course = super(StudentAddCourseSerializer, self).create(validated_data)
         lessons = Lesson.objects.filter(course=course)
-        student_course = StudentCourse.objects.create(
-                student=student, course=course, slug=course.slug
-        )
         for lesson in lessons:
-            article = Article.objects.get(lesson=lesson)
             student_lesson = StudentLesson.objects.create(
-                course=student_course, student=student, lesson=lesson
+                lesson=lesson, student=student, course=student_course
             )
 
-            if lesson.number == 1:
-                lock = True
-            else:
-                lock = False
+            articles = Article.objects.filter(lesson=lesson)
 
-            StudentArticle.objects.create(
-                article=article, student=student, lock=lock, lesson=student_lesson
-            )
+            for article in articles:
+                if article.number == 1:
+                    lock = True
+                else:
+                    lock = False
 
-        return validated_data
+                StudentArticle.objects.create(
+                    lesson=student_lesson, student=student, lock=lock, article=article
+                )
+
+        return student_course
