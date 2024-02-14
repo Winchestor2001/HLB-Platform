4e3e2a5396f29510df69c276e09bbeecd4b36bd4
Winchestor2001 <sbexruz2001@gmail.com>
2024-02-14 23:21:58 +0500

    update settings and env file

--- core/settings.py
+++ core/settings.py
@@ -77,23 +77,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DEPLOYMENT = env.bool('DEPLOYMENT')
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': env.str("DB_NAME"),
-#          'USER': env.str("DB_USER"),
-#          'PASSWORD': env.str("DB_PASSWORD"),
-#          'HOST': env.str("DB_HOST"),
-#          'PORT': env.int("DB_PORT"),
-#      }
-# }
+if DEPLOYMENT:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': env.str("DB_NAME"),
+            'USER': env.str("DB_USER"),
+            'PASSWORD': env.str("DB_PASSWORD"),
+            'HOST': env.str("DB_HOST"),
+            'PORT': env.int("DB_PORT"),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
